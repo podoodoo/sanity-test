cf67a052328216df4fc16de789ecef4535d85075
Tim <timyang@uw.edu>
2022-09-03 17:52:19 -0700

    basics

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
-import React from "react"
+import React, { useState } from "react"
 import { sanityClient, urlFor } from "./api/sanity"
 import { Post } from "../typings"
-import Image from "next/future/image"
-import { motion } from "framer-motion"
-import Link from "next/link"
 import { groq } from "next-sanity"
 import type { NextPage } from "next"
 
@@ -12,40 +9,8 @@ type Props = {
 }
 
 const Home: NextPage<Props> = ({ posts }) => {
-    return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 object-cover">
-            {posts.map((post, i) => (
-                <Link key={post._id} href={`/projects/${post.slug.current}`}>
-                    <motion.div
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        transition={{ delay: i * 0.2 }}
-                        className="border space-y-5 cursor-pointer h-full w-full hover:scale-105 ease-in-out transition-transform duration-200 p-5"
-                    >
-                        <Image
-                            src={urlFor(post.mainImage).url()}
-                            height="400"
-                            width="640"
-                            className="w-full h-2/3 object-scale-down"
-                            sizes="(min-width: 75em) 33vw,
-              (min-width: 48em) 50vw,
-              100vw"
-                            alt="post img"
-                        />
-                        <div className="p-5">
-                            <h3 className="text-lg">{post.title}</h3>
-                            <span
-                                className="text-xs"
-                                suppressHydrationWarning={true}
-                            >
-                                {new Date(post._createdAt).toDateString()}
-                            </span>
-                        </div>
-                    </motion.div>
-                </Link>
-            ))}
-        </div>
-    )
+    const [projectPosts, setProjectPosts] = useState<Post[]>(posts)
+    return <div className="container">{projectPosts[0].title}</div>
 }
 
 export const getServerSideProps = async () => {
